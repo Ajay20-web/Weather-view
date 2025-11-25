4ca21e04dafaa295df609fed151564fe79d6f2c4
Ajay <ajaykumar.ak20052001@gmail.com>
2025-11-25 22:05:36 +0530

    Add fetchLocation function to retrieve geographical data based on user input

---
+++ data/weather-api.js
@@ -0,0 +1,29 @@
+export async function fetchLocation(userLocationInput) {
+    let userLocation = userLocationInput || 'chennai'; 
+    try{ 
+    const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${userLocation}&count=1&language=en&format=json`);
+    if (!response.ok) {
+        throw new Error('Network response error' );
+    };
+
+    const data = await response.json();
+
+    if (!data.results || data.results.length === 0) {
+     alert("City not found. Try another name.");
+     return;
+   };
+
+    console.log(data);
+    const latitude = data.results[0].latitude;
+    const longitude = data.results[0].longitude;
+    console.log(latitude, longitude);
+ 
+} catch (error) {
+    console.log('Error fetching location:', error.message);
+    if ( error.message === `Cannot read properties of undefined (reading '0')` ) {
+    alert( 'Location not found. Please try again with a different city name.');
+    }else {
+    alert( error.message || 'Location not found. Please try again with a different city name.');
+    };
+};
+};
