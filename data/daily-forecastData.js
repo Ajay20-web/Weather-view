2c6be9115e9f2f73671d8718339569c0d0721744
Ajay <ajaykumar.ak20052001@gmail.com>
2025-11-28 21:55:27 +0530

    Add weather_code to daily forecast API request for getting icon owm code

--- data/daily-forecastData.js
+++ data/daily-forecastData.js
@@ -1,5 +1,5 @@
 export async function fetchDailyForecastData(latitude, longitude) {
-   const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=temperature_2m_max,temperature_2m_min&timezone=auto`
+   const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=temperature_2m_max,temperature_2m_min,weather_code&timezone=auto`;
     try {
         const response = await fetch(url);
         if (!response.ok) {
