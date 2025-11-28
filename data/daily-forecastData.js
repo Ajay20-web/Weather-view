64986131e4e798ed2b332ce7c641824ea408c6ae
Ajay <ajaykumar.ak20052001@gmail.com>
2025-11-28 21:22:18 +0530

    Add fetchDailyForecastData function to retrieve from api daily weather forecast based on latitude and longitude

---
+++ data/daily-forecastData.js
@@ -0,0 +1,15 @@
+export async function fetchDailyForecastData(latitude, longitude) {
+   const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=temperature_2m_max,temperature_2m_min&timezone=auto`
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Network response error');
+        };
+        const data = await response.json();
+       console.log(data);
+        return data;
+    } catch (error) {
+        console.log('Error fetching current weather data:', error.message);
+    };
+   
+};
