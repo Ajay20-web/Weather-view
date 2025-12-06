b08c834e963cd502b89caba8e6fe85e80e0e2740
Ajay <ajaykumar.ak20052001@gmail.com>
2025-12-06 19:55:56 +0530

    Integrating all module files to main file -> weather-view.js and creating the async function to handle all fetching

--- scripts/weather-view.js
+++ scripts/weather-view.js
@@ -1,31 +1,61 @@
 import {fetchLocation} from "../data/geocoding-api-data.js";
 import {fetchCurrentWeather} from "../data/current-weather-data.js";
-import './current-weather.js';
-import './daily-forecast.js';
-// Search button functionality
+import {fetchDailyForecastData} from "../data/daily-forecast-data.js";
+import {hourlyForecastData} from "../data/hourly-forecast-data.js";
+import {renderCurrentWeather} from "./current-weather.js";
+import {renderDailyForecast} from './daily-forecast.js';
+import  {renderHourlyForecast , hourlyForecastDataSlice} from"./hourly-forecast.js";
+
+//--Top Level variables for adding event to the select element for the hourly forecast this prevent the multiple event listener adding
+let topLevelWeatherData;
+let topLevelApiData;
+
+async function loadingAllFiles() {
+   document.querySelector('body').classList.add('loading');
+      //--Rendering the current weather data--
+      const {cityName , latitude, longitude} = await fetchLocation();
+      const weatherData = await fetchCurrentWeather(latitude, longitude);
+      renderCurrentWeather(cityName,weatherData);
+      //--Rendering the daily forecast data--
+      const foreCastData = await fetchDailyForecastData(latitude, longitude);
+      renderDailyForecast(foreCastData);
+      //--Rendering the hourly forecast data--
+      const {WeatherDataGrouping , apiData} = await hourlyForecastData(latitude, longitude);
+      topLevelWeatherData = WeatherDataGrouping;
+      topLevelApiData = apiData;
+      renderHourlyForecast(WeatherDataGrouping , apiData);
+   document.querySelector('body').classList.remove('loading');
+
+};
+//--Search button functionality--
 function searchButton() {
-    const searchBtn = document.querySelector(".js-search-btn");
-    searchBtn.addEventListener('click' , () => {
-       const userInput = document.querySelector('.js-search-bar');
-       const inputValue = userInput.value;
-       const searchString = inputValue.toLowerCase().trim();
-       if (!searchString) {
-        alert('Please enter a city name to search.');
-        return;
-       };
-       saveData(searchString);
-       fetchLocation();
-        fetchCurrentWeather();
-       userInput.value = '';
-    });
+   const searchBtn = document.querySelector(".js-search-btn");
+   searchBtn.addEventListener('click' , () => {
+      const userInput = document.querySelector('.js-search-bar');
+      const inputValue = userInput.value;
+      const searchString = inputValue.toLowerCase().trim();
+      if (!searchString) {
+      alert('Please enter a city name to search.');
+      return;
+      };
+      saveData(searchString);
+      userInput.value = '';
+      loadingAllFiles();
+   });
     
 };
-
-
+//--Save the user city into local storage--
 function saveData(userSearch) {
    localStorage.setItem('userInput',JSON.stringify(userSearch));
 };
+//--Global event listener for preventing the multiple event listener adding to select day element--
+const selector = document.querySelector(".js-selector");  
+selector.addEventListener('change', () => {
+   if (topLevelWeatherData && topLevelApiData) { 
+   const selectorValue = document.querySelector(".js-selector").value;
+   hourlyForecastDataSlice(topLevelWeatherData, topLevelApiData, selectorValue);
+   };
+});
 
+loadingAllFiles();
 searchButton();
- 
-
